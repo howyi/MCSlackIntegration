2f0f7a43c18dfc78d22c983919192e8c11527415
t-hayashi <t.hayashi.dev@gmail.com>
2023-08-20 02:36:07 +0900

    Init

---
+++ src/main/java/link/niwatori/slackchannelsrv/SlackChannelSRV.java
@@ -0,0 +1,17 @@
+package link.niwatori.slackchannelsrv;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SlackChannelSRV extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
