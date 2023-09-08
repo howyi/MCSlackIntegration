a20684b0e3cc8c5db2a71de73a3958f5abad5e5d
t-hayashi <t.hayashi.dev@gmail.com>
2023-09-09 01:26:43 +0900

    add aggressiveStrip

--- src/main/java/link/niwatori/slackintegration/LogAppender.java
+++ src/main/java/link/niwatori/slackintegration/LogAppender.java
@@ -3,6 +3,9 @@ package link.niwatori.slackintegration;
 import link.niwatori.slackintegration.message.Log;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.bukkit.ChatColor;
+
+import java.util.regex.Pattern;
 
 public class LogAppender extends AbstractAppender {
 
@@ -20,8 +23,14 @@ public class LogAppender extends AbstractAppender {
     public void append(LogEvent event) {
         if (this.config.consoleEnabled()) {
             LogEvent log = event.toImmutable();
-            Log message = new Log(this.config, log.getMessage().getFormattedMessage(), event.getLevel());
-            this.sender.sendMessage(message, this.config.consoleSlackChannelId());
+            String formattedMessage = log.getMessage().getFormattedMessage();
+            this.sender.sendMessage(new Log(this.config, aggressiveStrip(formattedMessage), event.getLevel()), this.config.consoleSlackChannelId());
         }
+    }
+
+    private static final Pattern aggressiveStripPattern = Pattern.compile("\u001B(?:\\[0?m|\\[38;2(?:;\\d{1,3}){3}m|\\[([0-9]{1,2}[;m]?){3})");
+
+    public static String aggressiveStrip(String text) {
+        return aggressiveStripPattern.matcher(text).replaceAll("");
     }
 }
